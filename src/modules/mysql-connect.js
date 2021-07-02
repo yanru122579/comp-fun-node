4bfaa911d507085d29931da2425d4c70e29f6a33
Ming Huang <a955269@gmail.com>
2021-07-02 15:02:28 +0800

    070221

--- src/modules/mysql-connect.js
+++ src/modules/mysql-connect.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   host: "localhost",
-  user: "root",
-  password: "",
-  database: "camping",
+  user: "dblink",
+  password: "campfun2021",
+  database: "campfun",
   waitForConnections: true,
   connectionLimit: 100, // 最大連線數
   queueLimit: 0,
