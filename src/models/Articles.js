dc16077b9a1902ea37c9a2f1dd98d4e3b887560d
alv1310 <symmttks99@gmail.com>
2021-07-03 18:42:51 +0800

    article add 2 files

--- src/models/Articles.js
+++ src/models/Articles.js
@@ -7,15 +7,80 @@ const db = require(__dirname + "/../modules/mysql2-connect");
 class Aticles {
   constructor(data) {
     // data: Object
-    let defaultData = {
-      sid: null,
-    };
-    this.data = { ...defaultData, ...data };
+    let defauleData = {
+      aId: null,
+      aTitle: '',
+      aImg: '',
+      author: '',
+      aContent: '',
+      aCategoryId: '',
+      aCommentId: '0',
+      aTagId: '0',
+      aDate: '1970-01-01',
+      created_at: '',
+      updated_at: '',
+  };
+  this.data = {...defauleData, ...data};
   }
-    static getTest() {
-      const r = {testkey:'這是情報誌api'}
-      console.log(r);
-      return r;
+
+
+    static async getRows(params={}){
+        let perPage = params.perPage || 5; //每頁有幾筆
+        let page = params.page || 1; // 查看第幾頁
+        let cate = parseInt(params.cate) || 0; // 分類編號
+        let keyword = params.keyword || ''; // 搜尋產品名稱或作者姓名
+        let orderBy = params.orderBy || ''; // 排序
+
+        let where = ' WHERE 1 ';
+        if(cate) {
+            where += ' AND aCategoryId='+ cate;
+        } // 類別
+        if(keyword){
+            let k2 = db.escape('%'+ keyword+ '%');
+            where += ` AND (author LIKE ${k2} OR aTitle LIKE ${k2})`
+        } // 搜尋
+
+        let orderStr = ''; // 排序
+        switch(orderBy){
+            case 'date-asc':
+                orderStr = ' ORDER BY `aDate` ASC';
+                break;
+            case 'date-desc':
+                orderStr = ' ORDER BY `aDate` DESC';
+                break;
+        }
+
+        let t_sql = `SELECT COUNT(1) num FROM \`articlelist\` ${where}`;
+        let [r1] = await db.query(t_sql);
+        let total = r1[0]['num']; // 共幾筆
+        
+        // 如有資料再執行此totalpages
+        let r2, totalPages=0;
+        if(total){
+            totalPages = Math.ceil(total/perPage);
+            let r_sql = `SELECT * FROM \`articlelist\` ${where} ${orderStr} LIMIT ${(page-1)*perPage}, ${perPage}`;
+            [r2] = await db.query(r_sql);
+        }
+        return {
+            total,
+            totalPages,
+            perPage,
+            page,
+            params,
+            data: r2,
+        }
+    }
+
+    // 讀取單筆 
+    static async getRow(aId){
+        if(!aId) return null;
+        let sql = "SELECT * FROM `articlelist` WHERE `aId`=?"
+        // 回傳取得單筆資料的陣列
+        let [r] = await db.query(sql, [aId]);
+        if(!r || !r.length){
+            return null;
+        }
+        return r[0]; // then retrieve object here
     }
 
 }
--- src/routes/articles.js
+++ src/routes/articles.js
@@ -8,8 +8,38 @@ const router = express.Router();
 
 //路由請由此開始設定
 //範例:
-router.get("/", async (req, res) => {
-  res.json(Articles.getTest(req.url));
+// router.get("/", async (req, res) => {
+//   res.json(Articles.getTest(req.url));
+// });
+
+// 取得全部
+router.get('/', async(req, res)=>{
+  res.json(await Articles.getRows());
 });
+
+
+// 取得單項
+router.get('/:aId', async(req, res)=>{
+  let p = await Articles.getRow(req.params.aId)    
+  res.json([req.baseUrl, req.url, p]);
+});
+
+
+// 取得類別 ( need to update )
+// router.get('/cate2', async(req, res)=>{
+//   res.json(await Articles.getRows({cate:2}));
+// });
+// router.get('/cate3', async(req, res)=>{
+//   res.json(await Articles.getRows({cate:3}));
+// });
+// router.get('/cate4', async(req, res)=>{
+//   res.json(await Articles.getRows({cate:4}));
+// });
+// router.get('/cate5', async(req, res)=>{
+//   res.json(await Articles.getRows({cate:5}));
+// });
+
+
+// 類別名稱
 
 module.exports = router;
