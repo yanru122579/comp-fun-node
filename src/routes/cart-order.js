2947d3364d28979de7fb5f4ef4d3b8654e4b94a4
Desh <eason77029@gmail.com>
2021-07-14 02:32:08 +0800

    2021/07/13- 產品/購物車API更新
    -產品新增多TAG查詢API
    -購物車修正部分API方法
    -產品SQL備份

--- src/routes/cart-order.js
+++ src/routes/cart-order.js
@@ -7,7 +7,7 @@ const upload = require(__dirname + "/../modules/upload-img");
 const router = express.Router();
 
 //訂單列表
-const totalorder = `SELECT cartOrder.cartOrderId,cartOrder.created_at,member.name,cartOrder.nNN,cartOrder.nAA,cartOrder.nCC,cartOrder.cartStatus,cartLogistics.cartLogisticsName,carPay.cartPayName,cartOrder.cartTotal,cartOrder.cartDescription FROM cartOrder INNER JOIN member ON cartOrder.mid = member.mId INNER JOIN carPay ON cartOrder.cartPayId = carPay.cartPayId INNER JOIN cartLogistics ON cartOrder.cartLogisticsId = cartLogistics.cartLogisticsId`;
+const totalorder = `SELECT cartOrder.cartOrderId,cartOrder.created_at,member.fName,cartOrder.nNN,cartOrder.nAA,cartOrder.nCC,cartOrder.cartStatus,cartLogistics.cartLogisticsName,carPay.cartPayName,cartOrder.cartTotal,cartOrder.cartDescription FROM cartOrder INNER JOIN member ON cartOrder.mid = member.mId INNER JOIN carPay ON cartOrder.cartPayId = carPay.cartPayId INNER JOIN cartLogistics ON cartOrder.cartLogisticsId = cartLogistics.cartLogisticsId`;
 
 //給訂單使用,查詢訂單頁面
 const getListData = async (req) => {
@@ -19,9 +19,22 @@ const getListData = async (req) => {
     page: 0,
     rows: [],
   };
+  //設定條件篩選 我拿來篩類別的 後面會用到可以先把要篩的都放這裡
+  const orderClass = req.query.orderClass;
+  const category = ` WHERE cartorder.orderclass = '${+orderClass}'`;
+  //設定交易狀態
+  const cartStatus = req.query.cartStatus;
+  const cartStatus1 = ` AND cartorder.cartStatus= '${cartStatus}'`;
+
+  //設定頁數
   let page = req.query.page || 1;
+  //將數字轉成整數
   page = parseInt(page);
+  //搜尋總表  看總筆數
   let t_sql = "SELECT COUNT(1) num FROM cartorder ";
+  //將上面的篩選類別拿下來使用 如果我的網址有類別 就總筆數where類別
+  orderClass ? (t_sql += category) : t_sql;
+  //去資料庫找總表的總筆數
   let [r1] = await db.query(t_sql);
   const perPage = 5; // 每頁要呈現幾筆資料
   const totalRows = r1[0].num; // 資料表的總筆數
@@ -36,21 +49,26 @@ const getListData = async (req) => {
       output.error = "page 值太大";
       return output;
     }
-    let sql = `SELECT cartOrder.cartOrderId,cartOrder.created_at,member.name,cartOrder.nNN,cartOrder.nAA,cartOrder.nCC,cartOrder.cartStatus,cartLogistics.cartLogisticsName,carPay.cartPayName,cartOrder.cartTotal,cartOrder.cartDescription,cartorder.orderclass FROM cartOrder INNER JOIN member ON cartOrder.mid = member.mId INNER JOIN carPay ON cartOrder.cartPayId = carPay.cartPayId INNER JOIN cartLogistics ON cartOrder.cartLogisticsId = cartLogistics.cartLogisticsId`;
+    //上面篩類別與總筆數頁數的設定  下面搜所有物品
+    //也是搜尋總表 但因為我要搭配購買商品 所以我join商品
+    let sql = `SELECT cartOrder.cartOrderId,cartOrder.created_at,member.fName,cartOrder.nNN,cartOrder.countries,cartOrder.townships,cartOrder.nAA,cartOrder.nCC,cartOrder.cartStatus,cartLogistics.cartLogisticsName,carPay.cartPayName,cartOrder.cartTotal,cartOrder.cartDescription,cartorder.orderclass FROM cartOrder INNER JOIN member ON cartOrder.mid = member.mId INNER JOIN carPay ON cartOrder.cartPayId = carPay.cartPayId INNER JOIN cartLogistics ON cartOrder.cartLogisticsId = cartLogistics.cartLogisticsId`;
+    //定義排序 用時間 最新到最舊 與設定頁數
     const limit = ` ORDER BY cartOrder.created_at DESC LIMIT ${
       (page - 1) * perPage
     }, ${perPage}`;
-    //設定條件篩選
-    const orderClass = req.query.orderClass;
-    const category = ` WHERE cartorder.orderclass = '${orderClass}'`;
+    //如果網址上有類別 就將sql+上where 去篩類別
     orderClass ? (sql += category) : sql;
-
+    cartStatus ? (sql += cartStatus1) : sql;
+    //sql 再加上排序與頁數的變數
     sql += limit;
+    //將sql去資料庫做搜尋 丟回[row]
     [rows] = await db.query(sql);
+    //以下是將時間做格式化處理
     rows.forEach((el) => {
       el.created_at = moment(el.created_at).format("YYYY-MM-DD");
     });
   }
+  //如果上面沒錯誤 就將 頁數 總筆數 搜尋總表放到output裏面
   if (!output.error) {
     output = {
       success: true,
@@ -61,21 +79,25 @@ const getListData = async (req) => {
       rows,
     };
   }
+  //回傳 output
   return output;
 };
 
+//這個api與上面的收尋做搭配 只要輸入http://localhost:4000/自己的路由/api  =會出現所有
+//http://localhost:4000/cartorder/api?/page=2   =會出現第二頁
+//http://localhost:4000/cartorder/api?/page=2&orderClass=3   =會出現第二頁且類別為３的東西
 //取得訂單API 訂單查詢頁面
 router.get("/api", async (req, res) => {
   res.json(await getListData(req));
 });
 
 //取得訂單商品詳細 訂單查詢頁面
-router.get("/item", async (req, res) => {
-  let t_sql =
-    "SELECT cartorder.cartorderId,cartitem.cartName,cartitem.cartName,cartitem.cartBuyQty,cartitem.cartBuyP FROM cartorder INNER JOIN cartitem ON cartorder.cartOrderId = cartitem.cartorderid WHERE cartorder.cartOrderId = 20210513175124";
-  let [r1] = await db.query(t_sql);
-  res.json(r1);
-});
+// router.get("/item", async (req, res) => {
+//   let t_sql =
+//     "SELECT cartorder.cartorderId,cartitem.cartName,cartitem.cartName,cartitem.cartBuyQty,cartitem.cartBuyP FROM cartorder INNER JOIN cartitem ON cartorder.cartOrderId = cartitem.cartorderid WHERE cartorder.cartOrderId = 20210513175124";
+//   let [r1] = await db.query(t_sql);
+//   res.json(r1);
+// });
 //查詢各項訂單的細節頁面
 router.get("/item/:cartOrderId", async (req, res) => {
   let r = [];
@@ -119,7 +141,7 @@ router.get("/item/:cartOrderId", async (req, res) => {
 router.post("/edit/:cartOrderId", upload.none(), async (req, res) => {
   const data = {
     ...req.body,
-    cartStatus: "已取消訂單",
+    cartStatus: "已取消",
   };
   console.log(data);
   let ouput = {
@@ -133,7 +155,6 @@ router.post("/edit/:cartOrderId", upload.none(), async (req, res) => {
   const [results] = await db.query(sql, [
     { cartStatus: data.cartStatus },
     req.params.cartOrderId,
-    req.params.nCC,
   ]);
   console.log({ cartStatus: data.cartStatus });
   output = results;
@@ -154,91 +175,133 @@ router.post("/add", upload.none(), async (req, res) => {
     error: "",
     insertId: 0,
   };
-  const data = {
-    ...req.body,
-    cartOrderId: moment().format("YYMMDDhhmmss"),
-  };
-  console.log(data);
-  const sql = "INSERT INTO `cartitem` SET ?";
-  const [results] = await db.query(sql, [
-    {
-      cartOrderId: data.cartOrderId,
-      product_id: data.product_id,
-      cartName: data.cartName,
-      cartBuyQty: data.cartBuyQty,
-      cartBuyP: data.cartBuyP,
-    },
-  ]);
+  // 期待前端傳送過來的資料結構
+  // req.body = {
+  //   orderItem: [
+  //   {}.
+  //   {},
+  // ],
+  //   orderInfo: {}
+  // }
+  // const cartOrderId = moment().format("YYMMDDhhmmss");
+  // 針對前端傳過來的item array做迴圈寫入資料庫處理
+  for (let item of req.body.orderItem) {
+    // item.cartOrderId = cartOrderId;
+    const sql = "INSERT INTO `cartitem` SET ?";
+    const [results] = await db.query(sql, [item]);
+    console.log(req.body);
+  }
+
   const sql1 = "INSERT INTO `cartorder` SET?";
   const [results1] = await db.query(sql1, [
     {
-      nNN: data.nNN,
-      nAA: data.nAA,
-      nCC: data.nCC,
-      nEE: data.nEE,
-      cartPayId: data.cartPayId,
-      cartLogisticsId: data.cartLogisticsId,
-      mid: data.mid,
-      cartTotal: data.cartTotal,
-      cartDescription: data.cartDescription,
-      cartStatus: data.cartStatus,
-      cartOrderId: data.cartOrderId,
+      nNN: req.body.orderInfo.nNN,
+      countries: req.body.orderInfo.countries,
+      townships: req.body.orderInfo.townships,
+      nAA: req.body.orderInfo.nAA,
+      nCC: req.body.orderInfo.nCC,
+      nEE: req.body.orderInfo.nEE,
+      cartPayId: req.body.orderInfo.cartPayId,
+      cartLogisticsId: req.body.orderInfo.cartLogisticsId,
+      mid: req.body.orderInfo.mid,
+      cartTotal: req.body.orderInfo.cartTotal,
+      cartDescription: req.body.orderInfo.cartDescription,
+      cartStatus: req.body.orderInfo.cartStatus,
+      orderclass: req.body.orderInfo.orderclass,
+      cartOrderId: req.body.orderInfo.cartOrderId,
     },
   ]);
-  output = { ...output, body: req.body };
+  output = { ...output, body: req.body.orderInfo };
   res.json(output);
 });
-
-//結帳時將使用者資訊與營地租借寫入資料庫
+//結帳時將使用者資訊與營地租借寫入資料庫 回圈版
 router.post("/addp", upload.none(), async (req, res) => {
   let output = {
     success: false,
     error: "",
     insertId: 0,
   };
-  const data = {
-    ...req.body,
-    cartOrderId: moment().format("YYMMDDhhmmss"),
-  };
-  console.log(data);
-  const sql = "INSERT INTO `cartplace` SET ?";
-  const [results] = await db.query(sql, [
-    {
-      cartOrderId: data.cartOrderId,
-      cpAreaId: data.cpAreaId,
-      cpArea: data.cpArea,
-      cpQty: data.cpQty,
-    },
-  ]);
+
+  // const cartOrderId = moment().format("YYMMDDhhmmss");
+  // 針對前端傳過來的item array做迴圈寫入資料庫處理
+  for (let item of req.body.orderItem) {
+    // item.cartOrderId = cartOrderId;
+    const sql = "INSERT INTO `cartplace` SET ?";
+    const [results] = await db.query(sql, [item]);
+    console.log(req.body);
+  }
+
   const sql1 = "INSERT INTO `cartorder` SET?";
   const [results1] = await db.query(sql1, [
     {
-      nNN: data.nNN,
-      nAA: data.nAA,
-      nCC: data.nCC,
-      nEE: data.nEE,
-      cartPayId: data.cartPayId,
-      cartLogisticsId: data.cartLogisticsId,
-      mid: data.mid,
-      cartTotal: data.cartTotal,
-      cartDescription: data.cartDescription,
-      cartStatus: data.cartStatus,
-      cartOrderId: data.cartOrderId,
-      orderclass: data.orderclass,
+      nNN: req.body.orderInfo.nNN,
+      nAA: req.body.orderInfo.nAA,
+      nCC: req.body.orderInfo.nCC,
+      nEE: req.body.orderInfo.nEE,
+      cartPayId: req.body.orderInfo.cartPayId,
+      cartLogisticsId: req.body.orderInfo.cartLogisticsId,
+      mid: req.body.orderInfo.mid,
+      cartTotal: req.body.orderInfo.cartTotal,
+      cartDescription: req.body.orderInfo.cartDescription,
+      cartStatus: req.body.orderInfo.cartStatus,
+      orderclass: req.body.orderInfo.orderclass,
+      cartOrderId: req.body.orderInfo.cartOrderId,
     },
   ]);
-  output = { ...output, body: req.body };
+  output = { ...output, body: req.body.orderInfo };
   res.json(output);
 });
+//結帳時將使用者資訊與營地租借寫入資料庫
+// router.post("/addp", upload.none(), async (req, res) => {
+//   let output = {
+//     success: false,
+//     error: "",
+//     insertId: 0,
+//   };
+//   const data = {
+//     ...req.body,
+//     cartOrderId: moment().format("YYMMDDhhmmss"),
+//   };
+//   console.log(data);
+//   const sql = "INSERT INTO `cartplace` SET ?";
+//   const [results] = await db.query(sql, [
+//     {
+//       cartOrderId: data.cartOrderId,
+//       cpAreaId: data.cpAreaId,
+//       cpArea: data.cpArea,
+//       cpQty: data.cpQty,
+//     },
+//   ]);
+//   const sql1 = "INSERT INTO `cartorder` SET?";
+//   const [results1] = await db.query(sql1, [
+//     {
+//       nNN: data.nNN,
+//       nAA: data.nAA,
+//       nCC: data.nCC,
+//       nEE: data.nEE,
+//       cartPayId: data.cartPayId,
+//       cartLogisticsId: data.cartLogisticsId,
+//       mid: data.mid,
+//       cartTotal: data.cartTotal,
+//       cartDescription: data.cartDescription,
+//       cartStatus: data.cartStatus,
+//       cartOrderId: data.cartOrderId,
+//       orderclass: +3,
+//     },
+//   ]);
+//   output = { ...output, body: req.body };
+//   res.json(output);
+// });
 
 //線下訂單確認系統
 router.get("/ordercheck/:cartOrderId/:nCC", async (req, res) => {
   let sql =
-    "SELECT cartOrder.cartOrderId,cartOrder.created_at,member.name,cartOrder.nNN,cartOrder.nAA,cartOrder.nCC,cartOrder.cartStatus,cartLogistics.cartLogisticsName,carPay.cartPayName,cartOrder.cartTotal,cartOrder.cartDescription,cartitem.cartName,cartitem.cartName,cartitem.cartBuyQty,cartitem.cartBuyP FROM cartOrder INNER JOIN member ON cartOrder.mid = member.mId INNER JOIN carPay ON cartOrder.cartPayId = carPay.cartPayId INNER JOIN cartLogistics ON cartOrder.cartLogisticsId = cartLogistics.cartLogisticsId INNER JOIN cartitem ON cartorder.cartOrderId = cartitem.cartOrderId WHERE cartOrder.cartOrderId = ? AND nCC = ?";
+    "SELECT cartOrder.cartOrderId,cartOrder.created_at,member.fName,cartOrder.nNN,cartOrder.countries,cartOrder.townships,cartOrder.nAA,cartOrder.nCC,cartOrder.cartStatus,cartLogistics.cartLogisticsName,carPay.cartPayName,cartOrder.cartTotal,cartOrder.cartDescription,cartitem.cartName,cartitem.cartName,cartitem.cartBuyQty,cartitem.cartBuyP FROM cartOrder INNER JOIN member ON cartOrder.mid = member.mId INNER JOIN carPay ON cartOrder.cartPayId = carPay.cartPayId INNER JOIN cartLogistics ON cartOrder.cartLogisticsId = cartLogistics.cartLogisticsId INNER JOIN cartitem ON cartorder.cartOrderId = cartitem.cartOrderId WHERE cartOrder.cartOrderId = ? AND nCC = ?";
   let [r] = await db.query(sql, [req.params.cartOrderId, req.params.nCC]);
   //如果沒有找到資料就轉向到列表頁
   if (!r.length) {
-    return res.redirect("/cartorder/ordercheck");
+    return res.json("找不到資料");
+    // return res.redirect("/cartorder/ordercheck");
   }
 
   res.json(r);
