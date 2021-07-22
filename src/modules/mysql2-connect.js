d338234e4895d8ad7d089dc6ad11964d8fb96f75
Ming Huang <a955269@gmail.com>
2021-07-22 13:38:04 +0800

    增加場地筆數,修改敘述圖片

--- src/modules/mysql2-connect.js
+++ src/modules/mysql2-connect.js
@@ -1,9 +1,11 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
+  // 網路版設定:請將host更換=> 184.168.103.196
+  // host: "184.168.103.196"
   host: "localhost",
-  user: "test",
-  password: "T1st@localhost",
+  user: "dblink",
+  password: "campfun2021",
   database: "campfun",
   waitForConnections: true,
   connectionLimit: 100, // 最大連線數
